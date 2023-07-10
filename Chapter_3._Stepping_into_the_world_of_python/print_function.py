a66a3f31a5d2c0379a7e97e0cfd909c9c35001eb
sjadhav4git <sjadhav4@buffalo.edu>
2023-07-10 05:58:56 -0400

    Print_function in python

---
+++ Chapter_3._Stepping_into_the_world_of_python/print_function.py
@@ -0,0 +1,15 @@
+# print function can takes n number of arguments and in differnt datatype.
+
+
+## print function takes one formal argument in the form of string 
+print("hello world!")                         
+
+## print function takes two formal arguments of an integer datatype
+print(1 + 2)                                   
+print(7 * 6)                                   
+
+## print function takes zero formal arguments
+print()
+
+## print function takes four formal arguments, with differnt datatypes (str), (int)                                        
+print("These String", "tooks", 4, "arguments") ## this is a tupple (str + int)
