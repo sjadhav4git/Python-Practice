5a836ed62c9c2f8a94005ad21f5be14b9fece332
sjadhav4git <sjadhav4@buffalo.edu>
2023-07-10 06:46:18 -0400

    escape char

---
+++ Chapter_3._Stepping_into_the_world_of_python/string_escape_character.py
@@ -0,0 +1,25 @@
+split_string = "this string has been\nsplit over\nseveral\nlines"
+print(split_string)
+
+tabbed_string = "1\t2\t3\t4\t5"
+print(tabbed_string)
+
+print('the pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
+
+print("the pet shop owner said \"No, no, 'e's uh,...he's resting\".")
+
+print("""the pet shop owner said "No, no, 'e's uh,...he's resting".""")
+
+another_split_string = """this string has been
+split over
+several
+lines"""
+
+print(another_split_string)
+
+another_split_string = """this string has been \
+split over \
+several \
+lines"""
+
+print(another_split_string)
