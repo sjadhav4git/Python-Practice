39f2b14c3592ac0b1db9cdf261be3650da6448c1
sjadhav4git <sjadhav4@buffalo.edu>
2023-07-10 06:09:13 -0400

    Python Strings

---
+++ Chapter_3._Stepping_into_the_world_of_python/strings.py
@@ -0,0 +1,14 @@
+# String concatenation, input function, variables in print function
+
+print("Today is a good day to learn")
+print('python if fun')
+print("python's string are easy to use")
+print('We can even include "quotes" in strings')
+print("hello" + " World")
+
+greetings = "Hello"
+name = input("please enter your name ")
+print(greetings + name)
+
+# we can add space between greetings and name
+print(greetings+' '+ name)
