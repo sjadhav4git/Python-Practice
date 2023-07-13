cdd4ab970c35dd4ea9a07b9e20dbbe6ef24476ad
sjadhav4git <sjadhav4@buffalo.edu>
2023-07-13 05:57:51 -0400

    String_indexing

---
+++ Chapter_3._Stepping_into_the_world_of_python/strings2.py
@@ -0,0 +1,9 @@
+#         0123456789.......
+parrot = "Norwegian Blue"
+
+print(parrot)
+print(parrot[3]) # w is at index 3 in string parrot
+
+#negative indexing
+print(parrot[-1])   # e is the at the -1 index in the string parrot
+print(parrot[-14])  # N
