50b0de8a461e74b8a8f236b909d5919c09f8ad34
sjadhav4git <sjadhav4@buffalo.edu>
2023-07-13 06:00:37 -0400

    indexing and slicing

--- Chapter_3._Stepping_into_the_world_of_python/strings2.py
+++ Chapter_3._Stepping_into_the_world_of_python/strings2.py
@@ -7,3 +7,11 @@ print(parrot[3]) # w is at index 3 in string parrot
 #negative indexing
 print(parrot[-1])   # e is the at the -1 index in the string parrot
 print(parrot[-14])  # N
+
+
+#========================
+
+# string slicing
+
+print(parrot[0:9]) # Norweg
+print(parrot[0:9:3])
