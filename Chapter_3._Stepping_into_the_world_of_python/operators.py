3318fb1f6b014f27b937654f5b3ad3157fbb1d84
sjadhav4git <sjadhav4@buffalo.edu>
2023-07-13 05:49:14 -0400

    Operator_Precedence

--- Chapter_3._Stepping_into_the_world_of_python/operators.py
+++ Chapter_3._Stepping_into_the_world_of_python/operators.py
@@ -8,3 +8,7 @@ print(a * b)
 print(a/b)      # output --> float
 print(a//b)     # output --> int (integer division, rounded down towards minus infinity)
 print(a%b)      # modulo: output --> remainder
+
+#operator precidance/ default execution sequence = division(/) OR multiplication(*) --> addition(+) OR substraction(-)
+# BODMAS = Brackets, Order(Exponents), Division/Multiplication, Addition/substraction 
+print(a+b / 3-4 * 12) # -35.0
