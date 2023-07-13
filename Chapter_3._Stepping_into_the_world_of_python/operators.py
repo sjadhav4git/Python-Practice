04761753cc0fc881e570316b3a684259dc5071d3
sjadhav4git <sjadhav4@buffalo.edu>
2023-07-13 05:42:08 -0400

    Arithmatic_operators

---
+++ Chapter_3._Stepping_into_the_world_of_python/operators.py
@@ -0,0 +1,10 @@
+a = 12
+b = 3
+
+# arithmatic operator
+print(a + b)
+print(a - b)
+print(a * b)
+print(a/b)      # output --> float
+print(a//b)     # output --> int (integer division, rounded down towards minus infinity)
+print(a%b)      # modulo: output --> remainder
